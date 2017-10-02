cc6ef4c5b053da239e1cfc401b3a00df4251abf9
Tharaka Ranatunga <tharaka.14@cse.mrt.ac.lk>
2017-10-02 18:33:32 +0530

    updating petsure controller

--- PetSure/Controllers/ValuesController.cs
+++ PetSure/Controllers/ValuesController.cs
@@ -7,7 +7,7 @@ using System.Web.Http;
 
 namespace PetSure.Controllers
 {
-    [Authorize]
+    //[Authorize]
     public class ValuesController : ApiController
     {
         // GET api/values
--- PetSure/Controllers/VetHubController.cs
+++ PetSure/Controllers/VetHubController.cs
@@ -7,16 +7,45 @@ using System.Web.Http;
 
 namespace PetSure.Controllers
 {
+    //[Authorize]
     public class VetHubController : ApiController
     {
-        public IEnumerable<VethubClaims> Get() {
-            PetSureEntities entities = new PetSureEntities();
-            {
-                //var product = products.FirstOrDefault((p) => p.Id == id);
-                var items = entities.VethubClaims;
-                return items.ToList();
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
 
-            }
+        // GET api/values/5
+        public string Get(int id)
+        {
+
+            PetSureEntities entities = new PetSureEntities();
+            var product = entities.VethubClaims.FirstOrDefault(p => p.PolicyNumber == id);
+
+
+            return "teer";
+
+            //{
+            //    var product = entities.VethubClaims.FirstOrDefault(p => p.PolicyNumber == amount);
+            //    return product;
+
+            //}
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
         }
     }
 }
